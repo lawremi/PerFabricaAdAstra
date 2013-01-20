ab66b762f192e37acf213926798945e3a628f49e
Michael Lawrence <michafla@gene.com>
2013-01-20 04:18:17 -0800

    Add missing GeoBlockAccessors interface.

---
+++ src/org/pfaa/geologica/block/GeoBlockAccessors.java
@@ -0,0 +1,14 @@
+package org.pfaa.geologica.block;
+
+import org.pfaa.block.CompositeBlockAccessors;
+import org.pfaa.geologica.GeoSubstance;
+
+public interface GeoBlockAccessors extends CompositeBlockAccessors {
+
+	public abstract GeoSubstance getSubstance(int meta);
+
+	public abstract boolean containsSubstance(GeoSubstance substance);
+
+	public abstract int getMetaForSubstance(GeoSubstance rock);
+
+}
