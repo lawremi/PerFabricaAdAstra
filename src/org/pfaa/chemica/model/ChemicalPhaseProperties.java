50a9eb385b1bc312aac29a1573c5f5adea0197fb
Michael Lawrence <michafla@gene.com>
2014-03-04 07:15:39 -0800

    refactor the ChemicalPhaseProperty constructors again in the never-ending effort to make the properties easier to define

--- src/org/pfaa/chemica/model/ChemicalPhaseProperties.java
+++ src/org/pfaa/chemica/model/ChemicalPhaseProperties.java
@@ -5,7 +5,7 @@ import java.awt.Color;
 public class ChemicalPhaseProperties extends PhaseProperties {
 	public final Thermo thermo;
 	
-	public ChemicalPhaseProperties(Color color, double density, Hazard hazard, Thermo thermo) {
+	public ChemicalPhaseProperties(Color color, double density, Thermo thermo, Hazard hazard) {
 		super(color, density, hazard);
 		this.thermo = thermo;
 	}
@@ -13,7 +13,7 @@ public class ChemicalPhaseProperties extends PhaseProperties {
 	public static class Solid extends ChemicalPhaseProperties {
 		public Solid(Color color, double density, Thermo thermo, Hazard hazard)
 		{
-			super(color, density, hazard, thermo);
+			super(color, density, thermo, hazard);
 		}
 		public Solid(Color color, double density, Thermo thermo)
 		{
@@ -21,37 +21,52 @@ public class ChemicalPhaseProperties extends PhaseProperties {
 		}
 		public Solid(double density, Thermo thermo, Hazard hazard)
 		{
-			super(Color.WHITE, density, hazard, thermo);
+			this(Color.WHITE, density, thermo, hazard);
+		}
+		public Solid(double density, Thermo thermo)
+		{
+			this(density, thermo, new Hazard());
 		}
 	}
 	
 	public static class Liquid extends ChemicalPhaseProperties {
 		public Liquid(Color color, double density, Thermo thermo, Hazard hazard)
 		{
-			super(color, density, hazard, thermo);
-		}
-		public Liquid(Color color, double density, Thermo thermo)
-		{
-			this(color, density, thermo, new Hazard());
+			super(color, density, thermo, hazard);
 		}
 		public Liquid(double density, Thermo thermo, Hazard hazard)
 		{
-			super(Color.BLUE, density, hazard, thermo);
+			this(null, density, thermo, hazard);
+		}
+		public Liquid(double density, Thermo thermo)
+		{
+			this(density, thermo, new Hazard());
+		}
+		public Liquid(Thermo thermo)
+		{
+			this(Double.NaN, thermo);
+		}
+		public Liquid() {
+			this(null);
 		}
 	}
 	
 	public static class Gas extends ChemicalPhaseProperties {
 		public Gas(Color color, Thermo thermo, Hazard hazard)
 		{
-			super(color, Double.NaN, hazard, thermo);
+			super(color, Double.NaN, thermo, hazard);
 		}
 		public Gas(Thermo thermo, Hazard hazard)
 		{
-			super(Color.WHITE, Double.NaN, hazard, thermo);
+			this(Color.WHITE, thermo, hazard);
 		}
 		public Gas(Thermo thermo)
 		{
 			this(thermo, new Hazard());
+		}
+		public Gas()
+		{
+			this(null);
 		}
 		
 		public static double getDensity(double molarMass, double temperature, double pressure) {
