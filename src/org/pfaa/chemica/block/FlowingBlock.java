2192500a7552d2da7e457857566608e01fd9622d
Michael Lawrence <michafla@gene.com>
2014-05-10 07:54:50 -0700

    add missing FlowingBlock

---
+++ src/org/pfaa/chemica/block/FlowingBlock.java
@@ -0,0 +1,13 @@
+package org.pfaa.chemica.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class FlowingBlock extends Block {
+
+	public FlowingBlock(int par1, Material par2Material) {
+		super(par1, par2Material);
+		// TODO Auto-generated constructor stub
+	}
+
+}
