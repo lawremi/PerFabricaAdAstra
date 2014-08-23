123d33727ce23d4edc12d69549a59cbaaa494431
Michael Lawrence <michafla@gene.com>
2014-08-22 21:01:56 -0700

    fix fog handling; overrides density (and mode) now

--- src/main/java/org/pfaa/chemica/client/fluid/FogHandler.java
+++ src/main/java/org/pfaa/chemica/client/fluid/FogHandler.java
@@ -1,11 +1,19 @@
 package org.pfaa.chemica.client.fluid;
 
 import net.minecraft.block.Block;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.potion.Potion;
 import net.minecraftforge.client.event.EntityViewRenderEvent.FogColors;
+import net.minecraftforge.client.event.EntityViewRenderEvent.FogDensity;
 import net.minecraftforge.fluids.BlockFluidBase;
+import net.minecraftforge.fluids.Fluid;
 
+import org.lwjgl.opengl.GL11;
 import org.pfaa.chemica.block.IndustrialFluidBlock;
 
+import codechicken.lib.math.MathHelper;
+
+import cpw.mods.fml.common.eventhandler.Event.Result;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class FogHandler {
@@ -29,4 +37,26 @@ public class FogHandler {
 	        event.blue = (color & 255) / 255.0F;
 		}
 	}
+
+	@SubscribeEvent
+	public void onFogDensity(FogDensity event) {
+		int x = (int)MathHelper.floor_double(event.entity.posX);
+		int y = (int)MathHelper.floor_double(event.entity.posY);
+		int z = (int)MathHelper.floor_double(event.entity.posZ);
+		Block block = event.entity.worldObj.getBlock(x, y, z);
+		if (block instanceof IndustrialFluidBlock) {
+			Fluid fluid = ((BlockFluidBase)block).getFluid();
+			GL11.glFogi(GL11.GL_FOG_MODE, GL11.GL_EXP);
+			if (!fluid.isGaseous() && event.entity.isPotionActive(Potion.waterBreathing))
+			{
+				event.density = 0.05F;
+			}
+			else
+			{
+				event.density = 0.1F - (float)EnchantmentHelper.getRespiration(event.entity) * 0.03F;
+			}
+			event.setCanceled(true);
+		}
+	}
+    
 }
