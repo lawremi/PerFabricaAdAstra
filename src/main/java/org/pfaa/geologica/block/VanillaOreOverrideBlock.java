fdea57d63d4879c86040028fc676f1514efa2740
Michael Lawrence <michafla@gene.com>
2014-08-22 19:27:17 -0700

    fix issue with manipulation of fields with obfuscated names

--- src/main/java/org/pfaa/geologica/block/VanillaOreOverrideBlock.java
+++ src/main/java/org/pfaa/geologica/block/VanillaOreOverrideBlock.java
@@ -1,25 +1,18 @@
 package org.pfaa.geologica.block;
 
-import cpw.mods.fml.relauncher.ReflectionHelper;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.ObfuscationReflectionHelper;
+import cpw.mods.fml.relauncher.ReflectionHelper;
 
 public class VanillaOreOverrideBlock extends Block {
-
-	
 	public VanillaOreOverrideBlock(Block vanillaOre) {
 		super(vanillaOre.getMaterial());
 		this.setCreativeTab(vanillaOre.getCreativeTabToDisplayOn());
-		this.setBlockTextureName((String)ReflectionHelper.getPrivateValue(Block.class, vanillaOre, "textureName"));
-		this.blockHardness = ReflectionHelper.getPrivateValue(Block.class, vanillaOre, "blockHardness");
-		this.blockResistance = ReflectionHelper.getPrivateValue(Block.class, vanillaOre, "blockResistance");
+		this.setBlockTextureName((String)ObfuscationReflectionHelper.getPrivateValue(Block.class, vanillaOre, "textureName"));
+		this.blockHardness = ObfuscationReflectionHelper.getPrivateValue(Block.class, vanillaOre, "blockHardness");
+		this.blockResistance = ObfuscationReflectionHelper.getPrivateValue(Block.class, vanillaOre, "blockResistance");
 		this.stepSound = vanillaOre.stepSound;
 		this.setHarvestLevel("pickaxe", (vanillaOre == Blocks.lapis_ore || vanillaOre == Blocks.iron_ore) ? 1 : 2);
 	}
-	
 }
