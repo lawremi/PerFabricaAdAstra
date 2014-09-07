f1b598b2ddd3e4d55e15a51828746b107121f5ba
Michael Lawrence <michafla@gene.com>
2014-09-07 06:49:24 -0700

    fix rendering of filled glass bottles

--- src/main/java/org/pfaa/chemica/fluid/FilledGlassBottleItem.java
+++ src/main/java/org/pfaa/chemica/fluid/FilledGlassBottleItem.java
@@ -3,16 +3,21 @@ package org.pfaa.chemica.fluid;
 import java.util.Collections;
 import java.util.List;
 
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemPotion;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidContainerRegistry;
 import net.minecraftforge.fluids.FluidStack;
 
+import org.pfaa.chemica.Chemica;
 import org.pfaa.chemica.fluid.IndustrialFluid;
+
+import com.google.common.base.CaseFormat;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -22,8 +27,12 @@ import cpw.mods.fml.relauncher.SideOnly;
  * maps from fluid to potion (damage on the itemstack), and vice versa. 
  * Otherwise, this class may behave strangely in a brewing stand.
  */
-public class FlaskItem extends ItemPotion {
+public class FilledGlassBottleItem extends ItemPotion {
 	
+    public FilledGlassBottleItem() {
+        this.setTextureName("potion");
+    }
+    
 	private Fluid getFluidForItemStack(ItemStack itemStack) {
 		FluidStack fluidStack = FluidContainerRegistry.getFluidForFilledItem(itemStack);
 		return fluidStack.getFluid();
@@ -40,6 +49,13 @@ public class FlaskItem extends ItemPotion {
 	}
 
 	@Override
+    @SideOnly(Side.CLIENT)
+    public boolean hasEffect(ItemStack p_77636_1_)
+    {
+        return false;
+    }
+
+    @Override
 	public List<PotionEffect> getEffects(ItemStack itemStack) {
 		Fluid fluid = this.getFluidForItemStack(itemStack);
 		if (fluid instanceof IndustrialFluid) {
@@ -55,6 +71,14 @@ public class FlaskItem extends ItemPotion {
 
 	@Override
 	public String getUnlocalizedName(ItemStack itemStack) {
-		return this.getUnlocalizedName() + "." + this.getFluidForItemStack(itemStack).getUnlocalizedName();
+	    String fluidPostfix = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, 
+	            this.getFluidForItemStack(itemStack).getName());
+		return this.getUnlocalizedName() + "." + fluidPostfix;
 	}
+
+    @Override
+    public String getItemStackDisplayName(ItemStack p_77653_1_)
+    {
+        return StatCollector.translateToLocal(this.getUnlocalizedName(p_77653_1_) + ".name").trim();
+    }
 }
