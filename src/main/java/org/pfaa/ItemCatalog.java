e2d56cc1447d0843fcba4970af025ed41b205701
Michael Lawrence <michafla@gene.com>
2014-09-07 06:48:29 -0700

    add/improve item and fluid container registration

--- src/main/java/org/pfaa/ItemCatalog.java
+++ src/main/java/org/pfaa/ItemCatalog.java
@@ -6,6 +6,8 @@ import net.minecraft.item.Item;
 
 import org.pfaa.geologica.Geologica;
 
+import com.google.common.base.CaseFormat;
+
 import cpw.mods.fml.common.LoaderException;
 
 public class ItemCatalog {
@@ -13,17 +15,11 @@ public class ItemCatalog {
 		T item = null;
 		try {
 			Constructor<T> constructor = itemClass.getConstructor();
-			String name = nameForItemClass(itemClass);
 			item = constructor.newInstance();
-			item.setUnlocalizedName(name);
 		} catch (Exception e) {
 			Geologica.log.fatal("Failed to construct item of class " + itemClass.getCanonicalName());
 			throw new LoaderException(e);
 		}
 		return item;
-	}
-	
-	private static String nameForItemClass(Class<? extends Item> itemClass) {
-		return itemClass.getSimpleName().replaceAll("Item$", "");
 	}
 }
