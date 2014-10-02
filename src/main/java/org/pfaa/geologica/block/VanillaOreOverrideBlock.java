9e5aebb86521402a288c5b2c7cd7de012629aa00
Michael Lawrence <michafla@gene.com>
2014-10-02 08:42:59 -0700

    temporary fix for texture path for vanilla ore override block

--- src/main/java/org/pfaa/geologica/block/VanillaOreOverrideBlock.java
+++ src/main/java/org/pfaa/geologica/block/VanillaOreOverrideBlock.java
@@ -10,10 +10,16 @@ public class VanillaOreOverrideBlock extends Block {
 	public VanillaOreOverrideBlock(Block vanillaOre) {
 		super(vanillaOre.getMaterial());
 		this.setCreativeTab(CreativeTabs.tabBlock);
-		this.setBlockTextureName((String)ReflectionHelper.getPrivateValue(Block.class, vanillaOre, 2));
+		super.setBlockTextureName((String)ReflectionHelper.getPrivateValue(Block.class, vanillaOre, 2));
 		this.blockHardness = ReflectionHelper.getPrivateValue(Block.class, vanillaOre, 20);
 		this.blockResistance = ReflectionHelper.getPrivateValue(Block.class, vanillaOre, 21);
 		this.stepSound = vanillaOre.stepSound;
 		this.setHarvestLevel("pickaxe", (vanillaOre == Blocks.lapis_ore || vanillaOre == Blocks.iron_ore) ? 1 : 2);
+	}
+
+	@Override
+	public Block setBlockTextureName(String p_149658_1_) {
+		// temporary hack until we move to overlays
+		return this;
 	}
 }
