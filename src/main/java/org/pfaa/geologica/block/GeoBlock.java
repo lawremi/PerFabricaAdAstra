fc388000323bbfef82dc3a3e8f6f6b69a536abf2
Michael Lawrence <michafla@gene.com>
2014-10-12 10:31:05 -0700

    use "GeoMaterial" instead of "Substance" in GeoBlock accessor names

--- src/main/java/org/pfaa/geologica/block/GeoBlock.java
+++ src/main/java/org/pfaa/geologica/block/GeoBlock.java
@@ -2,36 +2,69 @@ package org.pfaa.geologica.block;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.oredict.OreDictionary;
 
 import org.pfaa.block.CompositeBlock;
+import org.pfaa.chemica.model.Condition;
 import org.pfaa.chemica.model.IndustrialMaterial;
 import org.pfaa.geologica.GeoMaterial;
 import org.pfaa.geologica.GeoMaterial.Strength;
 import org.pfaa.geologica.Geologica;
+import org.pfaa.geologica.client.registration.ClientRegistrant;
 import org.pfaa.geologica.processing.Aggregate;
+import org.pfaa.geologica.processing.Aggregate.Aggregates;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+/* Rendering strategy:
+ * If the GeoMaterial has a host, then we render the host texture, with an overlay (32x32).
+ * We get the color from the GeoMaterial, and the overlay style depends on the
+ * strength and block Material (sand ore is 'wavy') of the GeoMaterial.
+ * For example: 
+ * * weak => "fragmented" -- kind of like emerald ore, but not shiny
+ * * medium => "diagonal scratches" -- like netherquartz
+ * * strong => "horizontal bands" -- like vanilla ores.
+ * BrickGeoBlock and BrokenGeoBlock will override to draw "crack" overlay.
+ * The same thing needs to happen for walls, stairs, slabs, etc.
+ */
 public abstract class GeoBlock extends CompositeBlock implements GeoBlockAccessors {
 
 	private List<GeoMaterial> materials = new ArrayList<GeoMaterial>();
 	
 	private Strength strength;
 	private Class<? extends IndustrialMaterial> composition;
+
+	private int renderPass;
+	private IIcon[] overlayIcons = new IIcon[4];
+	private static Set<Block> stoneBlocks;
+	private static Map<GeoMaterial, ItemStack> materialToNativeBlock = new HashMap();
 	
 	public GeoBlock(Strength strength, Class<? extends IndustrialMaterial> composition, Material material) {
 		super(material);
 		this.strength = strength;
 		this.composition = composition;
 		setCreativeTab(CreativeTabs.tabBlock);
-		setSubstances();
+		setGeoMaterials();
 		setHardness(determineHardness());
 		setResistance(determineResistance());
 		setStepSound(determineStepSound());
@@ -91,7 +124,7 @@ public abstract class GeoBlock extends CompositeBlock implements GeoBlockAccesso
 		return composition.isAssignableFrom(this.composition);
 	}
 
-	private void setSubstances() {
+	private void setGeoMaterials() {
 		List<GeoMaterial> materials = GeoMaterial.lookup(strength, composition, blockMaterial);
 		if (materials.size() > 16 || materials.size() < 1)
 			throw new IllegalArgumentException("GeoBlock only supports 1-16 materials");
@@ -99,7 +132,7 @@ public abstract class GeoBlock extends CompositeBlock implements GeoBlockAccesso
 		this.materials.addAll(materials);
 	}
 	
-	public List<GeoMaterial> getSubstances() {
+	public List<GeoMaterial> getGeoMaterials() {
 		return Collections.unmodifiableList(materials);
 	}
 
@@ -107,7 +140,7 @@ public abstract class GeoBlock extends CompositeBlock implements GeoBlockAccesso
 	 * @see org.pfaa.geologica.block.GeoBlockAccessors#getSubstance(int)
 	 */
 	@Override
-	public GeoMaterial getSubstance(int meta) {
+	public GeoMaterial getGeoMaterial(int meta) {
 		return materials.get(meta);
 	}
 
@@ -115,7 +148,7 @@ public abstract class GeoBlock extends CompositeBlock implements GeoBlockAccesso
 	 * @see org.pfaa.geologica.block.GeoBlockAccessors#containsSubstance(org.pfaa.geologica.GeoSubstance)
 	 */
 	@Override
-	public boolean containsSubstance(GeoMaterial material) {
+	public boolean containsGeoMaterial(GeoMaterial material) {
 		return materials.contains(material);
 	}
 	
@@ -131,7 +164,7 @@ public abstract class GeoBlock extends CompositeBlock implements GeoBlockAccesso
 
 	@Override
 	public String getBlockNameSuffix(int meta) {
-		return getSubstance(meta).getLowerName();
+		return getGeoMaterial(meta).getLowerName();
 	}
 
 	public ItemStack getItemStack(GeoMaterial material) {
