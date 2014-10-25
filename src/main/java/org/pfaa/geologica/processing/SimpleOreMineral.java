f38b95d7fd72ef57310c7c0012a8d64b2205741b
Michael Lawrence <michafla@gene.com>
2014-10-25 04:51:04 -0700

    add ability to override color at mineral level

--- src/main/java/org/pfaa/geologica/processing/SimpleOreMineral.java
+++ src/main/java/org/pfaa/geologica/processing/SimpleOreMineral.java
@@ -1,33 +1,41 @@
 package org.pfaa.geologica.processing;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.pfaa.chemica.model.Chemical;
 import org.pfaa.chemica.model.ChemicalPhaseProperties.Solid;
 import org.pfaa.chemica.model.CompoundDictionary;
+import org.pfaa.chemica.model.Condition;
+import org.pfaa.chemica.model.ConditionProperties;
 import org.pfaa.chemica.model.Formula;
 import org.pfaa.chemica.model.Mixture;
 import org.pfaa.chemica.model.MixtureComponent;
 import org.pfaa.chemica.model.SimpleChemical;
 
 public class SimpleOreMineral extends SimpleMineral implements OreMineral {
+	private Color overrideColor; /* crystal structures can have alternative colors */
+	
 	public SimpleOreMineral(Chemical concentrate, Substitution... substitutions) {
-		this(substitute(concentrate, substitutions));
+		this(concentrate, null, substitutions);
+	}
+	
+	public SimpleOreMineral(Chemical concentrate, Color overrideColor, Substitution... substitutions) {
+		this(substitute(concentrate, substitutions), overrideColor);
 	}
 	
 	public SimpleOreMineral(Mixture mixture) {
 		this(mixture.getComponents());
 	}
 	
-	public SimpleOreMineral(List<MixtureComponent> components) {
+	public SimpleOreMineral(List<MixtureComponent> components, Color overrideColor) {
 		super(components);
+		this.overrideColor = overrideColor;
 	}
 	
-	@Override
-	public OreMineral mix(Chemical material, double weight) {
-		Mixture mixture = super.mix(material, weight);
-		return new SimpleOreMineral(mixture.getComponents());
+	public SimpleOreMineral(List<MixtureComponent> components) {
+		this(components, null);
 	}
 	
 	@Override
@@ -57,5 +65,14 @@ public class SimpleOreMineral extends SimpleMineral implements OreMineral {
 			components.add(new MixtureComponent(compound, substitution.weight));
 		}
 		return components;
+	}
+
+	@Override
+	public ConditionProperties getProperties(Condition condition) {
+		ConditionProperties props = super.getProperties(condition);
+		if (this.overrideColor != null) {
+			props = props.recolor(this.overrideColor);
+		}
+		return props;
 	}
 }
