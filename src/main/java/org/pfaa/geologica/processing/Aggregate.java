af537f051ae262e22c5b4b707cbd2298f24de480
Michael Lawrence <michafla@gene.com>
2014-10-25 04:56:49 -0700

    aggregates now have color and density; add obsidian as aggregate

--- src/main/java/org/pfaa/geologica/processing/Aggregate.java
+++ src/main/java/org/pfaa/geologica/processing/Aggregate.java
@@ -1,21 +1,35 @@
 package org.pfaa.geologica.processing;
 
+import java.awt.Color;
 import java.util.Collections;
 import java.util.List;
 
 import org.pfaa.chemica.model.Condition;
 import org.pfaa.chemica.model.ConditionProperties;
+import org.pfaa.chemica.model.Hazard;
 import org.pfaa.chemica.model.IndustrialMaterial;
 import org.pfaa.chemica.model.Mixture;
 import org.pfaa.chemica.model.MixtureComponent;
+import org.pfaa.chemica.model.Phase;
 
 public interface Aggregate extends Mixture {
 	
 	public Aggregate mix(IndustrialMaterial material, double weight);
 	
 	public enum Aggregates implements Aggregate {
-		SAND, GRAVEL, STONE, CLAY, DIRT;
+		SAND(new Color(237, 201, 175), 1.6), 
+		GRAVEL(Color.gray, 1.2), 
+		STONE(Color.gray, 1.4), 
+		CLAY(Color.lightGray, 1.1), 
+		DIRT(new Color(150, 75, 0), 1.1),
+		OBSIDIAN(new Color(16, 16, 25), 2.5);
 
+		private ConditionProperties properties;
+		
+		private Aggregates(Color color, double density) {
+			this.properties = new ConditionProperties(Phase.SOLID, color, density, new Hazard());
+		}
+		
 		@Override
 		public String getOreDictKey() {
 			return name();
@@ -23,7 +37,7 @@ public interface Aggregate extends Mixture {
 
 		@Override
 		public ConditionProperties getProperties(Condition condition) {
-			return null;
+			return this.properties;
 		}
 
 		@Override
