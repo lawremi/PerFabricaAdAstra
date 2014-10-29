3a67b95526877a2ff22b7e6804c317361e634693
Michael Lawrence <michafla@gene.com>
2014-10-29 05:50:52 -0700

    enable multipass rendering for falling blocks

--- src/main/java/org/pfaa/geologica/client/registration/ClientRegistrant.java
+++ src/main/java/org/pfaa/geologica/client/registration/ClientRegistrant.java
@@ -1,6 +1,7 @@
 package org.pfaa.geologica.client.registration;
 
 import net.minecraft.block.Block;
+import net.minecraft.entity.item.EntityFallingBlock;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.IItemRenderer;
 import net.minecraftforge.client.MinecraftForgeClient;
@@ -9,7 +10,10 @@ import org.pfaa.block.CompositeBlockAccessors;
 import org.pfaa.geologica.GeologicaBlocks;
 import org.pfaa.geologica.block.StairsBlock;
 import org.pfaa.geologica.client.render.CompositeBlockItemRenderer;
+import org.pfaa.geologica.client.render.FallingCompositeBlockRenderer;
 import org.pfaa.geologica.registration.CommonRegistrant;
+
+import cpw.mods.fml.client.registry.RenderingRegistry;
 
 public class ClientRegistrant extends CommonRegistrant {
 	@Override
@@ -25,5 +29,6 @@ public class ClientRegistrant extends CommonRegistrant {
 				MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(block), renderer);
 			}
 		}
+		RenderingRegistry.registerEntityRenderingHandler(EntityFallingBlock.class, new FallingCompositeBlockRenderer());
 	}
 }
