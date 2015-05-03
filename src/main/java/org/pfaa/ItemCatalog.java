34eb669b466ae8a3c594f763c2d78bcf35b37e97
larman <michafla@gene.com>
2015-05-03 06:28:47 -0700

    remove unused item construction utility

--- src/main/java/org/pfaa/ItemCatalog.java
+++ src/main/java/org/pfaa/ItemCatalog.java
@@ -1,25 +1,6 @@
 package org.pfaa;
 
-import java.lang.reflect.Constructor;
-
-import net.minecraft.item.Item;
-
-import org.pfaa.geologica.Geologica;
-
-import com.google.common.base.CaseFormat;
-
-import cpw.mods.fml.common.LoaderException;
 
 public class ItemCatalog {
-	protected static <T extends Item> T createItem(Class<T> itemClass) {
-		T item = null;
-		try {
-			Constructor<T> constructor = itemClass.getConstructor();
-			item = constructor.newInstance();
-		} catch (Exception e) {
-			Geologica.log.fatal("Failed to construct item of class " + itemClass.getCanonicalName());
-			throw new LoaderException(e);
-		}
-		return item;
-	}
+	
 }
