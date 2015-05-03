c3684ad413b3007b628d14edd8de22ebedd516dd
larman <michafla@gene.com>
2015-05-03 06:45:23 -0700

    add lump and clump items

--- src/main/java/org/pfaa/geologica/GeologicaItems.java
+++ src/main/java/org/pfaa/geologica/GeologicaItems.java
@@ -1,15 +1,28 @@
 package org.pfaa.geologica;
 
-import net.minecraft.item.Item;
+import net.minecraft.block.material.Material;
 
 import org.pfaa.ItemCatalog;
 import org.pfaa.chemica.item.IndustrialMaterialItem;
-import org.pfaa.geologica.item.CrudeChunkItem;
-import org.pfaa.geologica.item.IndustrialMineralItem;
+import org.pfaa.chemica.processing.Form.Forms;
+import org.pfaa.geologica.processing.Crude;
+
+import com.google.common.base.Predicate;
 
 public class GeologicaItems extends ItemCatalog {
-	public static final IndustrialMaterialItem INDUSTRIAL_MINERAL_DUST = createItem(IndustrialMineralItem.class);
+	private static Predicate<GeoMaterial> EarthyMaterial = new Predicate<GeoMaterial>() {
+		public boolean apply(GeoMaterial obj) {
+			return obj.getBlockMaterial() == Material.clay || obj.getBlockMaterial() == Material.ground;
+		}
+	};
+	private static Predicate<GeoMaterial> CrudeRock = new Predicate<GeoMaterial>() {
+		public boolean apply(GeoMaterial obj) {
+			return obj.getComposition() instanceof Crude && obj.getBlockMaterial() == Material.rock;
+		}
+	};
 	
-	public static final Item CRUDE_CHUNK = createItem(CrudeChunkItem.class);
+	public static final IndustrialMaterialItem<GeoMaterial> CRUDE_LUMP = 
+			new IndustrialMaterialItem<GeoMaterial>(Forms.LUMP, GeoMaterial.class, CrudeRock);
+	public static final IndustrialMaterialItem<GeoMaterial> EARTHY_CLUMP = 
+			new IndustrialMaterialItem<GeoMaterial>(Forms.CLUMP, GeoMaterial.class, EarthyMaterial);
 }
-
--- src/main/java/org/pfaa/geologica/registration/ItemRegistration.java
+++ src/main/java/org/pfaa/geologica/registration/ItemRegistration.java
@@ -2,9 +2,11 @@ package org.pfaa.geologica.registration;
 
 import org.pfaa.RegistrationUtils;
 import org.pfaa.geologica.GeologicaBlocks;
+import org.pfaa.geologica.GeologicaItems;
 
 public class ItemRegistration {
 	public static void init() {
 		RegistrationUtils.registerContainersForDeclaredFluidBlocks(GeologicaBlocks.class);
+		RegistrationUtils.registerDeclaredItems(GeologicaItems.class);
 	}
 }
