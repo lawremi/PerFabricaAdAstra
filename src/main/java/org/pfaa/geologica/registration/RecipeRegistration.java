7b3b3f6f0f18d3b63ea106e959eaa2203a9f10a5
larman <michafla@gene.com>
2015-05-05 07:34:33 -0700

    crude lumps are now fuel

--- src/main/java/org/pfaa/geologica/registration/RecipeRegistration.java
+++ src/main/java/org/pfaa/geologica/registration/RecipeRegistration.java
@@ -47,6 +47,7 @@ import org.pfaa.geologica.processing.OreMineral.SmeltingTemperature;
 
 import com.google.common.base.CaseFormat;
 
+import cpw.mods.fml.common.IFuelHandler;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -421,4 +422,32 @@ public class RecipeRegistration {
 		drops.addChanceDrop(material, item, bonus, chance, fortuneMultiplies);
 	}
 
+	private static class CrudeFuelHandler implements IFuelHandler {
+
+		@Override
+		public int getBurnTime(ItemStack fuel) {
+			// FIXME: do this based on the chemical model later; for now it is hack city.
+			// Whatever we do, it should probably round to the nearest multiple of 200.
+			GeoMaterial material = GeologicaItems.CRUDE_LUMP.getIndustrialMaterial(fuel);
+			switch(material) {
+			case OIL_SHALE:
+				return 400;
+			case LIGNITE:
+				return 800;
+			case BITUMINOUS_COAL:
+				return 1600;
+			case ANTHRACITE:
+				return 2400;
+			case COAL_COKE:
+				return 3200;
+			default:
+				return 0;
+			}
+		}
+
+	}
+
+	private static void registerFuels() {
+		GameRegistry.registerFuelHandler(new CrudeFuelHandler());
+	}
 }
