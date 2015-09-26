a4d224b3862ddd71b1b649a0f2e1b3785a7cae1a
larman <michafla@gene.com>
2015-09-26 06:20:32 -0700

    BlockWithMeta gains hash code methods

--- src/main/java/org/pfaa/core/block/BlockWithMeta.java
+++ src/main/java/org/pfaa/core/block/BlockWithMeta.java
@@ -16,4 +16,20 @@ public class BlockWithMeta<B extends Block> {
 	public ItemStack getItemStack(int quantity) {
 		return new ItemStack(this.block, quantity, this.meta);
 	}
+	
+	@Override
+	public int hashCode()
+	{
+		return Block.getIdFromBlock(this.block) << Short.SIZE | this.meta & Short.MAX_VALUE;
+    }
+	
+	@Override
+	public boolean equals(Object o)
+	{
+		if (!(o instanceof BlockWithMeta)) return false;
+		BlockWithMeta<?> ok = (BlockWithMeta<?>)o;
+		if (this.block != ok.block) return false;
+		if (this.meta != ok.meta) return false;
+		return true;
+	}
 }
