33b4f4b9e7d4142479f71d82ee9381001b6c0cae
larman <michafla@gene.com>
2015-09-26 07:08:47 -0700

    add items for dust and crushed forms of ores

--- src/main/java/org/pfaa/geologica/GeologicaItems.java
+++ src/main/java/org/pfaa/geologica/GeologicaItems.java
@@ -26,9 +26,28 @@ public class GeologicaItems implements ItemCatalog {
 			return obj.getComposition() instanceof Crude && obj.getBlockMaterial() == Material.rock;
 		}
 	};
-	
+	private static Predicate<GeoMaterial> OreRock = new Predicate<GeoMaterial>() {
+		public boolean apply(GeoMaterial obj) {
+			return obj.getComposition() instanceof Ore && obj.getBlockMaterial() == Material.rock;
+		}
+	};
+
 	public static final IndustrialMaterialItem<GeoMaterial> CRUDE_LUMP = 
 			new IndustrialMaterialItem<GeoMaterial>(Forms.LUMP, GeoMaterial.class, CrudeRock);
 	public static final IndustrialMaterialItem<GeoMaterial> EARTHY_CLUMP = 
 			new IndustrialMaterialItem<GeoMaterial>(Forms.CLUMP, GeoMaterial.class, EarthyMaterial);
+	public static final IndustrialMaterialItem<GeoMaterial> ORE_CRUSHED = 
+			new IndustrialMaterialItem<GeoMaterial>(Forms.CRUSHED, GeoMaterial.class, OreRock);
+	public static final IndustrialMaterialItem<GeoMaterial> ORE_DUST = 
+			new IndustrialMaterialItem<GeoMaterial>(Forms.DUST, GeoMaterial.class, OreRock);
+	
+	public static final IndustrialMaterialItem<Ores> ORE_MINERAL_DUST = 
+			new IndustrialMaterialItem<Ores>(Forms.DUST, Ores.class);
+	public static final IndustrialMaterialItem<IndustrialMinerals> INDUSTRIAL_MINERAL_DUST = 
+			new IndustrialMaterialItem<IndustrialMinerals>(Forms.DUST, IndustrialMinerals.class);
+	
+	@SuppressWarnings("rawtypes")
+	public static List<IndustrialMaterialItem> getIndustrialMaterialItems() {
+		return CatalogUtils.getEntries(GeologicaItems.class, IndustrialMaterialItem.class);
+	}
 }
