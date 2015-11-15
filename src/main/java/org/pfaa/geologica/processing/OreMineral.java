ee96a27aead12e01205e998760f72e2f359d7cf3
larman <michafla@gene.com>
2015-11-14 21:09:32 -0800

    add electrum as mineral

--- src/main/java/org/pfaa/geologica/processing/OreMineral.java
+++ src/main/java/org/pfaa/geologica/processing/OreMineral.java
@@ -134,6 +134,7 @@ public interface OreMineral extends Mineral {
 		ZIRCON(Compounds.ZrSiO4, new Color(130, 60, 5)),
 		
 		GOLD(Au),
+		ELECTRUM(Alloys.ELECTRUM),
 		PLATINUM(Pt.mix(Pd, 1.0).mix(Ru, 0.25).mix(Rh, 0.2)
 				.mix(Ir, 0.01).mix(Alloys.IRIDOSMINE, 0.01)),
 		SULFUR(S)
