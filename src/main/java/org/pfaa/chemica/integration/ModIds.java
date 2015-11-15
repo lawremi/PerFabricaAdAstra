310db42f71e63d9ea17060c48d6e0aca702e078f
larman <michafla@gene.com>
2015-11-14 17:28:27 -0800

    add mod ID constants

--- src/main/java/org/pfaa/chemica/integration/ModIds.java
+++ src/main/java/org/pfaa/chemica/integration/ModIds.java
@@ -2,5 +2,14 @@ package org.pfaa.chemica.integration;
 
 public abstract class ModIds {
 	public static final String FORGE_MICROBLOCK = "ForgeMicroblock";
+	public static final String GREGTECH = "gregtech";
 	public static final String RAILCRAFT = "Railcraft";
+	public static final String APPLIED_ENERGISTICS_2 = "appliedenergistics2";
+	public static final String THERMAL_EXPANSION = "ThermalExpansion";
+	public static final String ENDER_IO = "EnderIO";
+	public static final String IMMERSIVE_ENGINEERING = "ImmersiveEngineering";
+	public static final String ROTARY_CRAFT = "RotaryCraft";
+	public static final String MAGNETICRAFT = "Magneticraft";
+	public static final String BUILDCRAFT_ADDITIONS = "bcadditions";
+	public static final String HYDRAULICRAFT = "HydCraft";
 }
