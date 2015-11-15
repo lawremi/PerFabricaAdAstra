9b479f46dd695805a6bf86d78bb920c32a70f217
larman <michafla@gene.com>
2015-11-14 17:55:26 -0800

    improvements to ChanceStack

--- src/main/java/org/pfaa/chemica/util/ChanceStack.java
+++ src/main/java/org/pfaa/chemica/util/ChanceStack.java
@@ -4,11 +4,26 @@ import net.minecraft.item.ItemStack;
 
 public class ChanceStack {
 	public final ItemStack itemStack;
-	public final double chance;
+	public final float chance;
 	
-	public ChanceStack(ItemStack stack, double chance) {
+	public ChanceStack(ItemStack stack, float chance) {
 		super();
-		this.itemStack = stack;
-		this.chance = chance;
+		this.itemStack = stack.copy();
+		this.chance = fudgeForUI(chance);
+	}
+	
+	private static float fudgeForUI(float chance) {
+		return chance + 0.00001F;
+	}
+
+	public ChanceStack weightChance(float weight) {
+		ItemStack itemStack = this.itemStack.copy();
+		float chance = this.chance;
+		itemStack.stackSize *= weight;
+		if (itemStack.stackSize == 0) {
+			itemStack.stackSize = 1;
+			chance *= weight * this.itemStack.stackSize;
+		}
+		return new ChanceStack(itemStack, chance);
 	}
 }
