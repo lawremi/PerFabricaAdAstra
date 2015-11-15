c05d7fb004024aaacb6bdd4e13c4f9d64128d087
larman <michafla@gene.com>
2015-11-14 17:28:35 -0800

    active mod integration

--- src/main/java/org/pfaa/chemica/integration/ModIntegration.java
+++ src/main/java/org/pfaa/chemica/integration/ModIntegration.java
@@ -1,10 +1,21 @@
 package org.pfaa.chemica.integration;
 
+import cpw.mods.fml.common.Loader;
+
 public abstract class ModIntegration {
 
 	public static void init() {
+		if (Loader.isModLoaded(ModIds.GREGTECH)) {
+			return; // we let Greg run the show
+		}
 		VanillaIntegration.init();
 		RailcraftIntegration.init();
+		AppliedEnergistics2Integration.init();
+		ThermalExpansionIntegration.init();
+		EnderIOIntegration.init();
+		ImmersiveEngineeringIntegration.init();
+		RotaryCraftIntegration.init();
+		BuildcraftAdditionsIntegration.init();
 	}
 	
 }
