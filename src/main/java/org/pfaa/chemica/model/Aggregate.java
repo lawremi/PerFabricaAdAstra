c227b04ee98bac8e7b466144d21bf0fb3d1349c6
larman <michafla@gene.com>
2015-11-14 17:29:11 -0800

    tweak Aggregate colors

--- src/main/java/org/pfaa/chemica/model/Aggregate.java
+++ src/main/java/org/pfaa/chemica/model/Aggregate.java
@@ -4,16 +4,18 @@ import java.awt.Color;
 import java.util.Collections;
 import java.util.List;
 
+import com.google.common.base.CaseFormat;
+
 public interface Aggregate extends Mixture {
 	
 	public Aggregate mix(IndustrialMaterial material, double weight);
 	
 	public enum Aggregates implements Aggregate {
-		SAND(new Color(237, 201, 175), 1.6), 
-		GRAVEL(Color.gray, 1.2), 
-		STONE(Color.gray, 1.4),
+		SAND(new Color(230, 220, 175), 1.6), 
+		GRAVEL(Color.lightGray, 1.2), 
+		STONE(Color.lightGray, 1.4),
 		CLAY(Color.lightGray, 1.1),
-		HARDENED_CLAY(Color.lightGray, 1.0),
+		HARDENED_CLAY(new Color(220, 185, 160), 1.0),
 		DIRT(new Color(150, 75, 0), 1.1),
 		OBSIDIAN(new Color(16, 16, 25), 2.5);
 
