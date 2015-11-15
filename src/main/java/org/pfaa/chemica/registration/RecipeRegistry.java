8926f1816de1e0a18153f9d7a2c3610b72efc310
larman <michafla@gene.com>
2015-11-14 17:18:07 -0800

    split casting and smelting recipe registration

--- src/main/java/org/pfaa/chemica/registration/RecipeRegistry.java
+++ src/main/java/org/pfaa/chemica/registration/RecipeRegistry.java
@@ -9,7 +9,8 @@ import org.pfaa.chemica.util.ChanceStack;
 import net.minecraft.item.ItemStack;
 
 public interface RecipeRegistry {
-	public void registerSmeltingRecipe(ItemStack input, ItemStack output, TemperatureLevel temp);
+	public void registerCastingRecipe(ItemStack input, ItemStack output, int temp);
+	public void registerSmeltingRecipe(ItemStack input, ItemStack output, ItemStack flux, TemperatureLevel temp);
 	public void registerGrindingRecipe(ItemStack input, ItemStack output, List<ChanceStack> secondaries, Strength strength);
 	public void registerCrushingRecipe(ItemStack input, ItemStack output, Strength strength);
 }
