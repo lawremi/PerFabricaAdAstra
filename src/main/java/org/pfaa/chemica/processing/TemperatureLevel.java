4b31c48d94eb024c42122b5d138c7fc1e6678aeb
larman <michafla@gene.com>
2015-11-14 17:54:56 -0800

    temperature level gains notion of reference temp

--- src/main/java/org/pfaa/chemica/processing/TemperatureLevel.java
+++ src/main/java/org/pfaa/chemica/processing/TemperatureLevel.java
@@ -1,5 +1,15 @@
 package org.pfaa.chemica.processing;
 
 public enum TemperatureLevel {
-	LOW, MEDIUM, HIGH, VERY_HIGH
+	LOW(800), MEDIUM(1200), HIGH(1600), VERY_HIGH(2000);
+	
+	private int referenceTemperature;
+	
+	private TemperatureLevel(int referenceTemperature) {
+		this.referenceTemperature = referenceTemperature;
+	}
+	
+	public int getReferenceTemperature() {
+		return this.referenceTemperature;
+	}
 }
