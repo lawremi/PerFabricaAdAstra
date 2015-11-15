340f02960ea5a4e9b9a051eeb95191b639bc0db2
larman <michafla@gene.com>
2015-11-14 18:12:24 -0800

    register containers for chemica fluids

--- src/main/java/org/pfaa/chemica/registration/ItemRegistration.java
+++ src/main/java/org/pfaa/chemica/registration/ItemRegistration.java
@@ -1,10 +1,12 @@
 package org.pfaa.chemica.registration;
 
+import org.pfaa.chemica.ChemicaBlocks;
 import org.pfaa.chemica.ChemicaItems;
 import org.pfaa.core.registration.RegistrationUtils;
 
 public class ItemRegistration {
     public static void init() {
+    	FluidRegistrationUtils.registerFluidContainers(ChemicaBlocks.class);
         RegistrationUtils.registerDeclaredItems(ChemicaItems.class);
     }
 }
