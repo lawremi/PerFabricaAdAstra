894694bc577a31452fa85d49e8655e3266bc0e70
larman <michafla@gene.com>
2015-11-14 17:26:44 -0800

    chemica uses custom renderer for composite items

--- src/main/java/org/pfaa/chemica/client/registration/ClientRegistrant.java
+++ src/main/java/org/pfaa/chemica/client/registration/ClientRegistrant.java
@@ -1,6 +1,26 @@
 package org.pfaa.chemica.client.registration;
 
+import org.pfaa.chemica.ChemicaBlocks;
+import org.pfaa.chemica.block.ConstructionMaterialBlock;
 import org.pfaa.chemica.registration.CommonRegistrant;
+import org.pfaa.core.client.render.CompositeBlockItemRenderer;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.client.IItemRenderer;
+import net.minecraftforge.client.MinecraftForgeClient;
 
 public class ClientRegistrant extends CommonRegistrant {
+	
+	@Override
+	public void register() {
+		super.register();
+		registerRenderers();
+	}
+	
+	private void registerRenderers() {
+		IItemRenderer renderer = new CompositeBlockItemRenderer();
+		for (ConstructionMaterialBlock block : ChemicaBlocks.getConstructionMaterialBlocks()) {
+			MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(block), renderer);
+		}
+	}
 }
