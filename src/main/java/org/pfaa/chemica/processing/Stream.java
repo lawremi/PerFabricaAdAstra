ecdde77c67fc628977fb3fa33fdc01461a44e593
larman <michafla@gene.com>
2015-11-14 17:53:44 -0800

    Stream becomes a class

--- src/main/java/org/pfaa/chemica/processing/Stream.java
+++ src/main/java/org/pfaa/chemica/processing/Stream.java
@@ -4,8 +4,31 @@ import org.pfaa.chemica.model.Condition;
 import org.pfaa.chemica.model.IndustrialMaterial;
 
 /* An industrial material in some condition and form */
-public interface Stream {
-	public IndustrialMaterial getIndustrialMaterial();
-	public Condition getCondition();
-	public Form getForm();
+public class Stream {
+	private IndustrialMaterial material;
+	private Condition condition;
+	private Form form;
+	
+	public Stream(IndustrialMaterial material, Form form) {
+		this(material, form, Condition.STP);
+	}
+	
+	public Stream(IndustrialMaterial material, Form form, Condition condition) {
+		super();
+		this.material = material;
+		this.condition = condition;
+		this.form = form;
+	}
+	
+	public IndustrialMaterial getIndustrialMaterial() {
+		return this.material;
+	}
+	
+	public Condition getCondition() {
+		return this.condition;
+	}
+	
+	public Form getForm() {
+		return this.form;
+	}
 }
