c29a5f9e0ba77c0f8060cb92cff98bd2b57f1545
larman <michafla@gene.com>
2016-03-23 21:30:52 -0700

    enable alpha blending when drawing overlay

--- src/main/java/org/pfaa/geologica/client/render/CompositeBlockRenderer.java
+++ src/main/java/org/pfaa/geologica/client/render/CompositeBlockRenderer.java
@@ -34,6 +34,7 @@ public class CompositeBlockRenderer implements ISimpleBlockRenderingHandler {
 		compositeBlock.disableOverlay();
 		flag = this.renderWorldBlockPass(x, y, z, block, renderer);
 		if (flag && compositeBlock.enableOverlay()) {
+			enableAlphaBlending();
 			this.renderWorldBlockPass(x, y, z, block, renderer);
 			compositeBlock.disableOverlay();
 		}
