71cdc96b9b33c0f1c04368d2d36c0b91f527ba6f
larman <michafla@gene.com>
2016-03-23 21:25:33 -0700

    register hook for catching fluid textures

--- src/main/java/org/pfaa/chemica/client/registration/ClientRegistrant.java
+++ src/main/java/org/pfaa/chemica/client/registration/ClientRegistrant.java
@@ -2,12 +2,14 @@ package org.pfaa.chemica.client.registration;
 
 import org.pfaa.chemica.ChemicaBlocks;
 import org.pfaa.chemica.block.ConstructionMaterialBlock;
+import org.pfaa.chemica.fluid.IndustrialFluids;
 import org.pfaa.chemica.registration.CommonRegistrant;
 import org.pfaa.core.client.render.CompositeBlockItemRenderer;
 
 import net.minecraft.item.Item;
 import net.minecraftforge.client.IItemRenderer;
 import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraftforge.common.MinecraftForge;
 
 public class ClientRegistrant extends CommonRegistrant {
 	
@@ -15,8 +17,13 @@ public class ClientRegistrant extends CommonRegistrant {
 	public void register() {
 		super.register();
 		registerRenderers();
+		registerHooks();
 	}
 	
+	private void registerHooks() {
+		MinecraftForge.EVENT_BUS.register(IndustrialFluids.getTextureHook());
+	}
+
 	private void registerRenderers() {
 		IItemRenderer renderer = new CompositeBlockItemRenderer();
 		for (ConstructionMaterialBlock block : ChemicaBlocks.getConstructionMaterialBlocks()) {
