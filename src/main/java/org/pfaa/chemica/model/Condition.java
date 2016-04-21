6baeb8305a3c87d8278e3993ab1dde19ad0c3c79
larman <michafla@gene.com>
2016-04-20 20:02:56 -0700

    change temp to int

--- src/main/java/org/pfaa/chemica/model/Condition.java
+++ src/main/java/org/pfaa/chemica/model/Condition.java
@@ -5,18 +5,21 @@ import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 
 public class Condition {
-	public double temperature;
+	public int temperature;
 	public double pressure;
 	
-	public Condition(double temperature, double pressure) {
+	public Condition(int temperature, double pressure) {
 		super();
 		this.temperature = temperature;
 		this.pressure = pressure;
 	}
 	
+	public Condition(int temperature) {
+		this(temperature, Constants.STANDARD_PRESSURE);
+	}
+	
 	public static Condition ofWorldCoordinates(World world, int x, int y, int z) {
-		double temperature = getTemperatureAt(world, x, y, z);
-		
+		int temperature = getTemperatureAt(world, x, y, z);
 		double pressure = getPressureAt(world, x, y, z);
 		return new Condition(temperature, pressure);
 	}
@@ -29,10 +32,10 @@ public class Condition {
 				                 (world.provider.getHeight() - sealevel);
 	}
 
-	private static float getTemperatureAt(World world, int x, int y, int z) {
+	private static int getTemperatureAt(World world, int x, int y, int z) {
 		BiomeGenBase biomegenbase = world.getBiomeGenForCoords(x, z);
 		float biometemp = biomegenbase.getFloatTemperature(x, y, z);
-		double temperature = (biometemp - 0.15F) * 45 / (2.0F - 0.15F);
+		int temperature = (int)((biometemp - 0.15F) * 45 / (2.0F - 0.15F));
 		boolean isHeated = world.getSavedLightValue(EnumSkyBlock.Block, x, y, z) == 10;
 		if (isHeated) {
 			temperature = Math.max(temperature, Constants.STANDARD_TEMPERATURE);
@@ -40,7 +43,7 @@ public class Condition {
 		// TODO: if near fire, we are >=100C; if near a fluid, we consider its temperature somehow. 
 		// But how to determine "near" 3x3? But should we allow for heat insulation somehow? 
 		// Maybe if surrounded by insulating blocks (or the same fluid)? 
-		return (float)temperature;
+		return temperature;
 	}
 
 	public static Condition STP = new Condition(Constants.STANDARD_TEMPERATURE, Constants.STANDARD_PRESSURE);
