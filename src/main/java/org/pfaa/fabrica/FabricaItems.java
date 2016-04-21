0dd8bbe76af15ae3fd30de690679c8d6a8f59792
larman <michafla@gene.com>
2016-04-20 20:36:13 -0700

    intermediate crushed => lump

--- src/main/java/org/pfaa/fabrica/FabricaItems.java
+++ src/main/java/org/pfaa/fabrica/FabricaItems.java
@@ -6,13 +6,19 @@ import org.pfaa.chemica.item.IndustrialMaterialItem;
 import org.pfaa.chemica.processing.Form.Forms;
 import org.pfaa.core.catalog.CatalogUtils;
 import org.pfaa.core.catalog.ItemCatalog;
+import org.pfaa.fabrica.item.JointCompoundItem;
 import org.pfaa.fabrica.model.Intermediate.Intermediates;
 
+import net.minecraft.item.Item;
+
 public class FabricaItems implements ItemCatalog {
-	public static final IndustrialMaterialItem<Intermediates> INTERMEDIATE_CRUSHED = 
-			new IndustrialMaterialItem<Intermediates>(Forms.CRUSHED, Intermediates.class);
+	
+	public static final IndustrialMaterialItem<Intermediates> INTERMEDIATE_LUMP = 
+			new IndustrialMaterialItem<Intermediates>(Forms.LUMP, Intermediates.class);
 	public static final IndustrialMaterialItem<Intermediates> INTERMEDIATE_DUST = 
 			new IndustrialMaterialItem<Intermediates>(Forms.DUST, Intermediates.class);
+	
+	public static final Item JOINT_COMPOUND = new JointCompoundItem();
 	
 	@SuppressWarnings("rawtypes")
 	public static List<IndustrialMaterialItem> getIndustrialMaterialItems() {
