1a27d6779c8501e9fcc119abf424e8d74a9315ce
larman <michafla@gene.com>
2016-04-25 18:39:15 -0700

    register hood tile entities

--- src/main/java/org/pfaa/fabrica/registration/EntityRegistration.java
+++ src/main/java/org/pfaa/fabrica/registration/EntityRegistration.java
@@ -2,6 +2,8 @@ package org.pfaa.fabrica.registration;
 
 import org.pfaa.chemica.integration.ModIds;
 import org.pfaa.fabrica.entity.TileEntityColored;
+import org.pfaa.fabrica.entity.TileEntityFurnaceVenting;
+import org.pfaa.fabrica.entity.TileEntityHood;
 
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -15,5 +17,7 @@ public class EntityRegistration {
 		if (Loader.isModLoaded(ModIds.OPEN_BLOCKS)) {
 			GameRegistry.registerTileEntity(TileEntityColored.class, "fabrica:colored");
 		}
+		GameRegistry.registerTileEntity(TileEntityHood.class, "fabrica:hood");
+		GameRegistry.registerTileEntity(TileEntityFurnaceVenting.class, "fabrica:furnaceVenting");
 	}
 }
