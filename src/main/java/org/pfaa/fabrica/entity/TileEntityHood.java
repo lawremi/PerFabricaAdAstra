54398314a0001b77b5932a5490a293c5fbc52f2a
larman <michafla@gene.com>
2016-04-25 18:36:15 -0700

    increase size of hood buffer since gases are bigger

--- src/main/java/org/pfaa/fabrica/entity/TileEntityHood.java
+++ src/main/java/org/pfaa/fabrica/entity/TileEntityHood.java
@@ -2,11 +2,17 @@ package org.pfaa.fabrica.entity;
 
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidContainerRegistry;
+import net.minecraftforge.fluids.FluidTank;
 import net.minecraftforge.fluids.IFluidHandler;
 import net.minecraftforge.fluids.TileFluidHandler;
 
 public class TileEntityHood extends TileFluidHandler implements IFluidHandler {
 
+	public TileEntityHood() {
+		this.tank = new FluidTank(FluidContainerRegistry.BUCKET_VOLUME * 20);
+	}
+	
 	public boolean canUpdate()
     {
         return false;
