cba6796a1b8f4783ae384a83bd65de90f4e9497e
larman <michafla@gene.com>
2016-11-19 04:21:32 -0800

    register fluid reactor block

--- src/main/java/org/pfaa/fabrica/FabricaBlocks.java
+++ src/main/java/org/pfaa/fabrica/FabricaBlocks.java
@@ -3,6 +3,7 @@ package org.pfaa.fabrica;
 import org.pfaa.chemica.integration.ModIds;
 import org.pfaa.core.catalog.BlockCatalog;
 import org.pfaa.fabrica.block.DrywallBlock;
+import org.pfaa.fabrica.block.FluidReactorBlock;
 import org.pfaa.fabrica.block.HoodBlock;
 import org.pfaa.fabrica.block.PaintableDrywallBlock;
 
@@ -12,6 +13,7 @@ import net.minecraft.block.Block;
 public class FabricaBlocks implements BlockCatalog {
 	public static final Block DRYWALL;
 	public static final Block HOOD = new HoodBlock();
+	public static final Block FLUID_REACTOR = new FluidReactorBlock();
 	
 	static {
 		if (Loader.isModLoaded(ModIds.OPEN_BLOCKS)) { 
