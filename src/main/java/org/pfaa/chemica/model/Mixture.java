194744c1b4fdbd29331799942eabab38352c504d
larman <michafla@gene.com>
2016-11-19 03:51:54 -0800

    depend on Java 8, drop MixtureUtils, use default methods on Mixture

--- src/main/java/org/pfaa/chemica/model/Mixture.java
+++ src/main/java/org/pfaa/chemica/model/Mixture.java
@@ -1,7 +1,54 @@
 package org.pfaa.chemica.model;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public interface Mixture extends IndustrialMaterial {
-	public List<MixtureComponent> getComponents();
+	List<MixtureComponent> getComponents();
+	
+	default MixtureComponent getComponent(IndustrialMaterial material) {
+		for (MixtureComponent comp : this.getComponents()) {
+			if (comp.material.equals(material)) {
+				return comp;
+			}
+		}
+		return null;
+	}
+	
+	default Mixture concentrate(Mixture mixture, int factor) {
+		List<MixtureComponent> comps = mixture.getComponents();
+		return new SimpleMixture(comps.stream().map((comp) -> {
+			if (comp.weight < 1.0)
+				return comp.concentrate(factor);
+			return comp;
+		}).collect(Collectors.toList()));
+	}
+	
+	default Extraction extract(IndustrialMaterial extractant, IndustrialMaterial... materials) {
+		Mixture extract = new SimpleMixture(extractant);
+		Mixture residuum = new SimpleMixture();
+		for (IndustrialMaterial material : materials) {
+			MixtureComponent comp = this.getComponent(material);
+			if (comp != null) {
+				extract.mix(comp);
+			}
+		}
+		List<IndustrialMaterial> materialList = Arrays.asList(materials);
+		for (MixtureComponent comp : this.getComponents()) {
+			if (!materialList.contains(comp.material)) {
+				residuum.mix(comp);
+			}
+		}
+		return new Extraction(extractant, extract, residuum);
+	}
+	
+	default double getTotalWeight() {
+		double weight = 0;
+		for (MixtureComponent comp : this.getComponents()) {
+			weight += comp.weight;
+		}
+		return weight;
+	}
+
 }
