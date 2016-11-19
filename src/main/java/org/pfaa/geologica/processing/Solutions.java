e682e8b2cfec3bd507a42091eaea886c22f134d6
larman <michafla@gene.com>
2016-11-19 03:57:31 -0800

    preserve name of purified brine

--- src/main/java/org/pfaa/geologica/processing/Solutions.java
+++ src/main/java/org/pfaa/geologica/processing/Solutions.java
@@ -2,8 +2,12 @@ package org.pfaa.geologica.processing;
 
 import org.pfaa.chemica.model.Compound.Compounds;
 import org.pfaa.chemica.model.Mixture;
+import org.pfaa.chemica.model.MixtureComponent;
 import org.pfaa.chemica.model.SimpleMixture;
 
 public class Solutions {
-	public static final Mixture PURIFIED_BRINE = new SimpleMixture("brine.purified", Compounds.H2O).mix(Compounds.NaCl, 0.3);
+	public static final Mixture PURIFIED_BRINE = 
+			new SimpleMixture("brine.purified", 
+					new MixtureComponent(Compounds.H2O, 1.0), 
+					new MixtureComponent(Compounds.NaCl, 0.3));
 }
