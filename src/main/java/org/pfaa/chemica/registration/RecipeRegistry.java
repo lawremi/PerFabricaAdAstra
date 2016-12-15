3f354b1aafa18ceb2ce5f6e42647041f8976cfb0
larman <michafla@gene.com>
2016-12-15 05:03:47 -0800

    add cooling recipes, crystallization->precipitation

--- src/main/java/org/pfaa/chemica/registration/RecipeRegistry.java
+++ src/main/java/org/pfaa/chemica/registration/RecipeRegistry.java
@@ -12,19 +12,21 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 
 public interface RecipeRegistry {
-	// FIXME: Should we use an abstract cost/energy value instead of temperature/condition?
-	//        Right now, we cannot incorporate heat capacities and phase transitions.
+	// FIXME: Standardize all methods to have required condition and energy (where necessary)
 	public void registerCastingRecipe(ItemStack input, ItemStack output, ItemStack flux, int temp);
 	public void registerCastingRecipe(FluidStack input, ItemStack output);
 	public void registerMeltingRecipe(ItemStack input, FluidStack output, int temp);
 	public void registerCoolingRecipe(FluidStack input, ItemStack output, int heat);
-	public void registerCrystallizationRecipe(FluidStack input, ItemStack output, int cost);
+	public void registerCoolingRecipe(FluidStack input, FluidStack output, int heat);
+	public void registerPrecipitationRecipe(FluidStack input, ItemStack output, int cost);
 	// FIXME: both smelting functions should take a FluidStack gas parameter for e.g. SO2 capture when smelting sulfides
 	public void registerSmeltingRecipe(ItemStack input, ItemStack output, ItemStack flux, TemperatureLevel temp);
 	public void registerSmeltingRecipe(ItemStack input, FluidStack output, ItemStack flux, TemperatureLevel temp);
 	public void registerGrindingRecipe(ItemStack input, ItemStack output, List<ChanceStack> secondaries, Strength strength);
 	public void registerCrushingRecipe(ItemStack input, ItemStack output, ChanceStack dust, Strength strength);
 	public void registerPhysicalSeparationRecipe(ItemStack input, List<ChanceStack> outputs);
+	// TODO: add FluidStack vapor parameter to facilitate 2-stage implementations
+	//       - note that the vapor might actually be liquid if cryogenic -- maybe call it "columnInput"?
 	public void registerDistillationRecipe(FluidStack input, List<FluidStack> outputs, Condition condition);
 	public void registerAlloyingRecipe(ItemStack output, ItemStack base, List<ItemStack> solutes, int temp);
 	public void registerAlloyingRecipe(FluidStack output, List<FluidStack> inputs);
