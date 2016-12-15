6f4ef3bdd3ef0e4aa975b8c7af36a4ec58ba78ca
larman <michafla@gene.com>
2016-12-15 04:58:21 -0800

    refactor decompositions, support bicarbonates

--- src/main/java/org/pfaa/chemica/registration/ReactionFactory.java
+++ src/main/java/org/pfaa/chemica/registration/ReactionFactory.java
@@ -1,14 +1,12 @@
 package org.pfaa.chemica.registration;
 
 import org.pfaa.chemica.model.Compound;
+import org.pfaa.chemica.model.Compound.Compounds;
 import org.pfaa.chemica.model.Formula;
-import org.pfaa.chemica.model.Formula.Part;
 import org.pfaa.chemica.model.Ion;
 import org.pfaa.chemica.model.Ion.Ions;
 import org.pfaa.chemica.model.Reaction;
 import org.pfaa.chemica.model.State;
-import org.pfaa.chemica.model.Compound.Compounds;
-import org.pfaa.chemica.model.Element;
 
 public class ReactionFactory {
 	
@@ -31,38 +29,37 @@ public class ReactionFactory {
 	}
 
 	public static Reaction makeStandardSaltDecompositionReaction(Compound compound) {
-		Part CO3 = new Part(Element.C, Element.O._(3)),
-		     OH2 = new Part(Element.O, Element.H)._(2),
-		     OH = new Part(Element.O, Element.H),
-		     NO3 = new Part(Element.N, Element.O._(3)),
-		     NO32 = new Part(Element.N, Element.O._(3))._(2),
-		     NO2 = new Part(Element.N, Element.O._(2));
 		Formula formula = compound.getFormula();
 		boolean secondaryCarbonate = 
 				formula.getParts().size() == 3 && 
-				formula.getParts().get(1).hasComposition(CO3);
+				formula.getParts().get(1).hasComposition(Ions.CO3);
 		if (!secondaryCarbonate && formula.getParts().size() > 2) {
 			return null;
 		}
-		Part last = formula.getLastPart();
+		Ion anion = formula.getAnion();
+		int stoich = formula.getLastPart().stoichiometry;
 		Reaction reaction = null;
-		if (last.equals(CO3)) {
+		if (anion == Ions.CO3) {
 			reaction = Reaction.of(compound).yields(RecipeRegistration.oxide(formula)).and(Compounds.CO2);
-		} else if (last.equals(NO3)) {
+		} else if (anion == Ions.NO3 && stoich == 1) {
 			if (formula.getCation() == Ions.Li) {
 				reaction = Reaction.of(4, compound).yields(2, RecipeRegistration.oxide(formula)).and(4, Compounds.NO2).and(Compounds.O2);
 			} else {
-				Formula metalNitriteFormula = formula.withLastPart(NO2);
+				Formula metalNitriteFormula = formula.withAnion(Ions.NO2);
 				Compound metalNitrite = Compounds.valueOf(metalNitriteFormula.toString());
 				reaction = Reaction.of(2, compound).yields(2, metalNitrite).and(Compounds.O2);
 			}
-		} else if (last.equals(NO32)) {
+		} else if (anion == Ions.NO3 && stoich == 2) {
 			reaction = Reaction.of(2, compound).yields(2, RecipeRegistration.oxide(formula)).and(4, Compounds.NO2).and(Compounds.O2);
-		} else if (last.equals(OH2)) {
+		} else if (anion == Ions.OH && stoich == 2) {
 			int oxideStoich = formula.getFirstPart().stoichiometry;
 			reaction = Reaction.of(compound).yields(oxideStoich, RecipeRegistration.oxide(formula)).and(1, Compounds.H2O, State.GAS);
-		} else if (last.equals(OH)) {
+		} else if (anion == Ions.OH) {
 			reaction = Reaction.of(2, compound).yields(RecipeRegistration.oxide(formula)).and(1, Compounds.H2O, State.GAS);
+		} else if (anion == Ions.HCO3) {
+			Formula metalCarbonateFormula = formula.withAnion(Ions.CO3).withCation(formula.getCation());
+			Compound metalCarbonate = Compounds.valueOf(metalCarbonateFormula.toString());
+			reaction = Reaction.of(2, compound).yields(metalCarbonate).and(1, Compounds.H2O, State.GAS).and(Compounds.CO2);
 		}
 		if (reaction != null && secondaryCarbonate) {
 			reaction = reaction.and(formula.getParts().get(1).stoichiometry, Compounds.CO2);
