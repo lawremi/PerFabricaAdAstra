4a271dd15e21967be34e01eed13991ecad9a29b5
larman <michafla@gene.com>
2016-12-15 04:53:54 -0800

    add some thermodynamic calculations

--- src/main/java/org/pfaa/chemica/model/Chemical.java
+++ src/main/java/org/pfaa/chemica/model/Chemical.java
@@ -8,13 +8,23 @@ public interface Chemical extends IndustrialMaterial, Vaporizable, Fusible, Solu
 	public Formula getFormula();
 	
 	default double getEnthalpyOfFusion() {
-		Condition cond = this.getFusion().getCondition();
+		Condition cond = this.getFusion().getLiquidCondition();
 		ChemicalConditionProperties liquid = this.getProperties(cond, State.LIQUID);
 		ChemicalConditionProperties solid = this.getProperties(cond, State.SOLID);
 		if (solid == null || liquid == null) {
 			return Double.NaN;
 		}
 		return (liquid.entropy - solid.entropy) * cond.temperature;
+	}
+	
+	default double getEnthalpyOfVaporization() {
+		Condition cond = this.getVaporization().getGasCondition();
+		ChemicalConditionProperties liquid = this.getProperties(cond, State.LIQUID);
+		ChemicalConditionProperties gas = this.getProperties(cond, State.GAS);
+		if (gas == null || liquid == null) {
+			return Double.NaN;
+		}
+		return (gas.entropy - liquid.entropy) * cond.temperature;
 	}
 	
 	default Reaction getDissociation() {
@@ -27,5 +37,16 @@ public interface Chemical extends IndustrialMaterial, Vaporizable, Fusible, Solu
 		return Reaction.inWaterOf(1, this, State.SOLID).
 				yields(cation.stoichiometry, cation.ion).
 				and(anion.stoichiometry, anion.ion);
+	}
+	
+	default double getEnthalpyChange(Condition to) {
+		return this.getEnthalpyChange(Condition.STP, to);
+	}
+	
+	default double getEnthalpyChange(Condition from, Condition to) {
+		// TODO: if the enthalpy is NaN, look at adjacent state and add heat of transition, which we know if we know the entropy
+		ChemicalConditionProperties fromProps = this.getProperties(from);
+		ChemicalConditionProperties toProps = this.getProperties(to);
+		return toProps.enthalpy - fromProps.enthalpy;
 	}
 }
