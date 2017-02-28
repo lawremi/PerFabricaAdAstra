7bc48fc2e50bdea9c9ee7ef5764f9ed85de9b609
larman <michafla@gene.com>
2017-02-28 06:12:53 -0800

    IngredientStack gains getChance(), with() IngredientList constructor

--- src/main/java/org/pfaa/chemica/item/IngredientStack.java
+++ src/main/java/org/pfaa/chemica/item/IngredientStack.java
@@ -1,6 +1,9 @@
 package org.pfaa.chemica.item;
 
+import java.util.Arrays;
 import java.util.Set;
+
+import org.pfaa.chemica.registration.IngredientList;
 
 import net.minecraft.item.ItemStack;
 
@@ -9,5 +12,11 @@ public interface IngredientStack {
 	public ItemStack getBestItemStack();
 	public Set<ItemStack> getItemStacks();
 	public int getSize();
+	default float getChance() { return 1F; }
 	public Object getCraftingIngredient();
+	default IngredientList<IngredientStack> with(IngredientStack... stacks) {
+		IngredientList<IngredientStack> ingredients = IngredientList.of(this);
+		ingredients.addAll(Arrays.asList(stacks));
+		return ingredients;
+	}
 }
