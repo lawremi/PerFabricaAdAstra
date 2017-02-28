87485d59ed51a3b20abf5b2a715de608fdcbf5b6
larman <michafla@gene.com>
2017-02-28 05:51:07 -0800

    aqueous considered fluid, State knows volume scalar, add ofMatter()

--- src/main/java/org/pfaa/chemica/model/State.java
+++ src/main/java/org/pfaa/chemica/model/State.java
@@ -6,9 +6,22 @@ public enum State {
 	SOLID, LIQUID, GAS, AQUEOUS;
 	
 	public boolean isFluid() {
-		return this == LIQUID || this == GAS;
+		return this == LIQUID || this == GAS || this == AQUEOUS;
 	}
 
+	public float getVolumeFactor() {
+		if (this == State.GAS) { 
+			return 100;
+		} else if (this == State.AQUEOUS) {
+			return (1 - Constants.STANDARD_SOLUTE_WEIGHT) / Constants.STANDARD_SOLUTE_WEIGHT;
+		}
+		return 1;
+	}
+	
+	public State ofMatter() {
+		return this == State.AQUEOUS ? State.LIQUID : this;
+	}
+	
 	public <T extends IndustrialMaterial> MaterialState<T> of(T material) {
 		return MaterialState.of(this, material);
 	}
