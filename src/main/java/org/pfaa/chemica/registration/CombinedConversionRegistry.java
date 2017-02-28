2d3685ab3a7625c6f9e15b34f6c7b4a6b5ab5948
larman <michafla@gene.com>
2017-02-28 06:47:06 -0800

    use Streams for looping over ConversionRegistries

--- src/main/java/org/pfaa/chemica/registration/CombinedConversionRegistry.java
+++ src/main/java/org/pfaa/chemica/registration/CombinedConversionRegistry.java
@@ -6,11 +6,14 @@ import java.util.Map;
 import java.util.Set;
 
 import org.pfaa.chemica.model.Reaction;
-import org.pfaa.chemica.processing.Stacking;
+import org.pfaa.chemica.processing.Alloying;
 import org.pfaa.chemica.processing.Combination;
+import org.pfaa.chemica.processing.Communition;
+import org.pfaa.chemica.processing.Compaction;
 import org.pfaa.chemica.processing.EnthalpyChange;
 import org.pfaa.chemica.processing.Separation;
-import org.pfaa.chemica.processing.Sizing;
+import org.pfaa.chemica.processing.Smelting;
+import org.pfaa.chemica.processing.Stacking;
 
 public class CombinedConversionRegistry implements ConversionRegistry {
 
@@ -30,44 +33,45 @@ public class CombinedConversionRegistry implements ConversionRegistry {
 
 	@Override
 	public void register(Stacking agglomeration) {
-		for (ConversionRegistry registry : registries.values()) {
-			registry.register(agglomeration);
-		}
+		registries.values().forEach((registry) -> registry.register(agglomeration));
 	}
 
 	@Override
 	public void register(Separation separation) {
-		for (ConversionRegistry registry : registries.values()) {
-			registry.register(separation);
-		}
+		registries.values().forEach((registry) -> registry.register(separation));
 	}
 
+	@Override
+	public void register(Alloying alloying) {
+		registries.values().forEach((registry) -> registry.register(alloying));
+	}
+	
+	public void register(Smelting smelting) {
+		registries.values().forEach((registry) -> registry.register(smelting));
+	}
+	
 	@Override
 	public void register(Combination combination) {
-		for (ConversionRegistry registry : registries.values()) {
-			registry.register(combination);
-		}
-		
+		registries.values().forEach((registry) -> registry.register(combination));
 	}
 
 	@Override
-	public void register(Sizing sizing) {
-		for (ConversionRegistry registry : registries.values()) {
-			registry.register(sizing);
-		}		
+	public void register(Communition sizing) {
+		registries.values().forEach((registry) -> registry.register(sizing));		
+	}
+
+	@Override
+	public void register(Compaction sizing) {
+		registries.values().forEach((registry) -> registry.register(sizing));		
 	}
 
 	@Override
 	public void register(EnthalpyChange enthalpyChange) {
-		for (ConversionRegistry registry : registries.values()) {
-			registry.register(enthalpyChange);
-		}
+		registries.values().forEach((registry) -> registry.register(enthalpyChange));
 	}
 
 	@Override
 	public void register(Reaction reaction) {
-		for (ConversionRegistry registry : registries.values()) {
-			registry.register(reaction);
-		}		
+		registries.values().forEach((registry) -> registry.register(reaction));		
 	}
 }
