ca6c7dcefa341865224126e77eb04aad0adf79a4
larman <michafla@gene.com>
2017-03-04 03:47:19 -0800

    remove some dead code

--- src/main/java/org/pfaa/chemica/processing/Compaction.java
+++ src/main/java/org/pfaa/chemica/processing/Compaction.java
@@ -1,9 +1,5 @@
 package org.pfaa.chemica.processing;
 
-import java.util.Arrays;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
-
 import org.pfaa.chemica.model.Condition;
 import org.pfaa.chemica.model.IndustrialMaterial;
 
@@ -35,13 +31,5 @@ public class Compaction extends DegenerateConversion implements Sizing {
 
 	public static Compaction of(IndustrialMaterial material) {
 		return new Compaction(material);
-	}
-	
-	public static <T extends Enum<?> & IndustrialMaterial> Stream<Compaction> of(Class<T> material, Predicate<T> predicate) {
-		return Arrays.stream(material.getEnumConstants()).filter(predicate).map(Compaction::of);
-	}
-	
-	public static <T extends Enum<?> & IndustrialMaterial> Stream<Compaction> of(Class<T> material) {
-		return of(material, (x) -> true);
 	}
 }
