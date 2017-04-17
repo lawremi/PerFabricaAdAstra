9dbc6a686f813ea7826075b90c9e2a9424377ce1
larman <michafla@gene.com>
2017-04-17 04:08:11 -0700

    rework communition
    - "bonus" outputs are returned via getOutputs(), downweighed by 1/9
    - add notion of reduction: a reducing communition yields the first
      mixture component (plus bonuses), instead of the mixture itself
    - if a material is implicitly granular, always yield block form

--- src/main/java/org/pfaa/chemica/processing/Communition.java
+++ src/main/java/org/pfaa/chemica/processing/Communition.java
@@ -1,16 +1,37 @@
 package org.pfaa.chemica.processing;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import org.pfaa.chemica.model.IndustrialMaterial;
+import org.pfaa.chemica.model.Mixture;
+import org.pfaa.chemica.processing.Form.Forms;
+
+import com.google.common.collect.Lists;
 
 public class Communition extends DegenerateConversion implements Sizing {
+	private boolean reduces;
+	
 	protected Communition(IndustrialMaterial material) {
 		super(material);
 	}
 
+	private IndustrialMaterial getOutputMaterial() {
+		if (this.reduces && this.getMaterial() instanceof Mixture) {
+			Mixture mixture = (Mixture)this.getMaterial();
+			return mixture.getComponents().get(0).material;
+		} else {
+			return this.getMaterial();
+		}
+	}
+	
+	public Communition reduces() {
+		this.reduces = true;
+		return this;
+	}
+	
 	@Override
 	public Direction getDirection() {
 		return Direction.DECREASE;
@@ -18,13 +39,28 @@ public class Communition extends DegenerateConversion implements Sizing {
 
 	@Override
 	public Form getOutputForm(Form inputForm) {
+		if (this.getOutputMaterial().isGranular())
+			return Forms.BLOCK;
 		return inputForm.communite();
 	}
 
 	@Override
-	public Stream<MaterialStoich<?>> getBonusOutputs(Form form) {
-		// TODO Get components from mixture, scaling by output form
-		return super.getBonusOutputs(form);
+	public List<MaterialStoich<?>> getOutputs() {
+		List<MaterialStoich<?>> outputs = super.getOutputs();
+		if (this.getMaterial() instanceof Mixture) { 
+			Mixture mixture = (Mixture)this.getMaterial();
+			if (this.reduces) {
+				outputs = Lists.newArrayList(MaterialStoich.of(mixture.getComponents().get(0)));
+			} else {
+				outputs = Lists.newArrayList(outputs);
+			}
+			mixture.getComponents().stream().
+					skip(1).
+					map(MaterialStoich::of).
+					map((s) -> s.scale(1/9)).
+					forEach(outputs::add);
+		}
+		return outputs;
 	}
 
 	public static Communition of(IndustrialMaterial material) {
