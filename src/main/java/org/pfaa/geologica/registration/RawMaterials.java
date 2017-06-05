28ffe67ab159d20b9ce2ecdbde0dc81b84fad97b
larman <michafla@gene.com>
2017-06-05 05:32:48 -0700

    generalize RawMaterial mapping beyond ores

--- src/main/java/org/pfaa/geologica/registration/RawMaterials.java
+++ src/main/java/org/pfaa/geologica/registration/RawMaterials.java
@@ -6,26 +6,26 @@ import java.util.List;
 import java.util.Map;
 
 import org.pfaa.chemica.model.Chemical;
+import org.pfaa.chemica.model.IndustrialMaterial;
 import org.pfaa.geologica.GeoMaterial;
-import org.pfaa.geologica.processing.Mineral;
 import org.pfaa.geologica.processing.OreMineral;
 
 public class RawMaterials {
 	private static Map<Chemical,OreMineral> oreCompoundToMineral = new HashMap<Chemical, OreMineral>();
-	private static Map<OreMineral,List<GeoMaterial>> oreMineralToGeoMaterials = new HashMap<OreMineral,List<GeoMaterial>>();
+	private static Map<IndustrialMaterial,List<GeoMaterial>> materialToGeoMaterials = 
+			new HashMap<IndustrialMaterial,List<GeoMaterial>>();
 	
 	public static void register(OreMineral mineral) {
 		oreCompoundToMineral.put(mineral.getConcentrate(), mineral);
 	}
 	
 	public static void register(GeoMaterial material) {
-		Mineral concentrate = material.getOreConcentrate();
-		if (concentrate instanceof OreMineral) {
-			OreMineral mineral = (OreMineral) concentrate;
-			List<GeoMaterial> materials = oreMineralToGeoMaterials.get(mineral);
+		IndustrialMaterial concentrate = material.getConcentrate();
+		if (concentrate != null) {
+			List<GeoMaterial> materials = materialToGeoMaterials.get(concentrate);
 			if (materials == null) {
 				materials = new ArrayList<GeoMaterial>();
-				oreMineralToGeoMaterials.put(mineral, materials);
+				materialToGeoMaterials.put(concentrate, materials);
 			}
 			materials.add(material);
 		}
@@ -35,7 +35,7 @@ public class RawMaterials {
 		return oreCompoundToMineral.get(chemical);
 	}
 	
-	public static List<GeoMaterial> get(OreMineral mineral) {
-		return oreMineralToGeoMaterials.get(mineral);
+	public static List<GeoMaterial> get(IndustrialMaterial material) {
+		return materialToGeoMaterials.get(material);
 	}
 }
